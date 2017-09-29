7533c31f3cf6c04aa82f60c6a4976d219fd04af2
designfirst <designfirst@gmail.com>
2017-09-29 18:56:49 -0400

    cleanup and removed unused js

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,46 +23,57 @@ $(document).ready(function() {
 	    },
 
 	    setupScenes: function() {
+	    	var controller = new ScrollMagic.Controller({globalSceneOptions:{refreshInterval: 0}});               
 
-// , globalSceneOptions: {duration: 0, reverse: false, refreshInterval: 0}
-	    	var controller = new ScrollMagic.Controller({loglevel: 0, refreshInterval: 0});
+            // show articles in article listing
+            $(".article-listing .snippet").each(function(index) {
+                var snippet = this;
+                var delay;
+
+                if (index === 2 ) {
+                    delay = "-50";
+                } else {
+                    delay = "-200";
+                }
+
+                new ScrollMagic.Scene({triggerHook: 1, triggerElement: snippet, offset: delay, reverse: false})
+                    .setClassToggle(this, "snippet--reveal")
+                    // .addIndicators({name: "snippet"})
+                    .addTo(controller)
+            });
 
             // show recirc articles in article footer
-            new ScrollMagic.Scene({triggerElement: ".article__content__footer", offset: -100, reverse: false, refreshInterval: 0})
+            new ScrollMagic.Scene({triggerHook: 1, triggerElement: ".article__content__footer", offset: 400, reverse: false})
                 .setClassToggle(".recirc__articles__item", "reveal")
-                // .addIndicators({name: "1 (duration: 0)"})
-                .addTo(controller);
+                // .addIndicators({name: "recirc"})
+                .addTo(controller)
 
             // show article share tools
-			new ScrollMagic.Scene({triggerElement: ".article__content__title", offset: 800, reverse: false, refreshInterval: 0})
+			new ScrollMagic.Scene({triggerHook: 0.05, triggerElement: ".article__content__title", reverse: false})
 	            .setClassToggle(".share-buttons__link-item", "share-buttons__link-item--reveal")
                 .addTo(controller);
 
             // Scale sticky header
-            new ScrollMagic.Scene({triggerElement: ".content", duration: 200, offset: 350, refreshInterval: 0})
-                .setTween(".site-header", {height: "65px", boxShadow: "rgb(204, 204, 204) 0px 1px 3px;"},)
-                // .addIndicators({name: "1 (duration: 200)"})
+            new ScrollMagic.Scene({triggerHook: 0.05, triggerElement: ".content", duration: 100, offset: 0, reverse: true})
+                .setTween(".site-header", {height: "65px", boxShadow: "rgb(204, 204, 204) 0px 1px 3px;"})
+                // .addIndicators({name: "header (duration: 100)"})
                 .addTo(controller);
-
-            new ScrollMagic.Scene({triggerElement: ".content", duration: 100, offset: 300, refreshInterval: 0})
-                .setTween(".site-header__logo", {scale: 0.7, margin: "10px 0 10px 20px"})
-                // .setClassToggle(".site-header__logo", "site-header__logo--scale")
-                // .addIndicators({name: "2 (duration: 200)"})
+            new ScrollMagic.Scene({triggerHook: 0.05, triggerElement: ".content", duration: 0, offset: 0, reverse: true})
+                .setClassToggle(".site-header__logo", "site-header__logo--scaled")
+                // .addIndicators({name: "header__logo (duration: 0)"})
                 .addTo(controller);
-
-            new ScrollMagic.Scene({triggerElement: ".content", duration: 200, offset: 400, refreshInterval: 0})
+            new ScrollMagic.Scene({triggerHook: 0.05, triggerElement: ".content", duration: 100, offset: 0, reverse: true})
                 .setTween(".navigation", {margin: "10px 30px 0 0"})
-                // .addIndicators({name: "3 (duration: 200)"})
+                // .addIndicators({name: "navigation (duration: 100)"})
                 .addTo(controller);
         },
 
         updateArticleCSS: function() {
             var delay = 50;
             var offset = 100;
-            var $articles = $(".recirc__articles__item");
 
-            _.each($articles, function(recircItem, index) {
-                $(recircItem).css({
+            $(".recirc__articles__item").each(function(){
+                $(this).css({
                     'animation-delay': delay + 'ms'
                 });
 
@@ -73,10 +84,9 @@ $(document).ready(function() {
 	    updateShareButtonsCSS: function() {
             var delay = 50;
             var offset = 100;
-            var $shareButtons = $(".share-buttons__link-item");
-
-            _.each($shareButtons, function(button, index) {
-				$(button).css({
+            
+            $(".share-buttons__link-item").each(function() {
+				$(this).css({
 	                'animation-delay': delay + 'ms'
 	            });
 
