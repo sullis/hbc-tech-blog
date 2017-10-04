624668a6d5e0cd698b4b2c195568520a1adc6957
designfirst <designfirst@gmail.com>
2017-10-04 14:53:21 -0400

    removed jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,13 @@
 /* scripts */
+document.addEventListener('DOMContentLoaded',function() {
 
-$(document).ready(function() {
-
-	var App = {
+    var App = {
 
         init: function() {
             this.bindEvents();
-            LazyLoadRecircArticles.init();
+            LazyLoadRecircArticles.setupScenes();
+            LazyLoadRecircArticles.articleAnimationTiming();
+            LazyLoadRecircArticles.randomizeArticleHeaderColors();
         },
 
         bindEvents: function() {
@@ -16,18 +17,14 @@ $(document).ready(function() {
 
     var LazyLoadRecircArticles = {
 
-	    init: function() {
-	    	this.setupScenes();
-            this.articleAnimationTiming();
-	    	this.randomizeArticleHeaderColors();
-	    },
-
-	    setupScenes: function() {
-	    	var controller = new ScrollMagic.Controller({globalSceneOptions:{refreshInterval: 0}});               
+        setupScenes: function() {
+            var controller = new ScrollMagic.Controller({globalSceneOptions:{refreshInterval: 0}});               
 
             // show articles in article listing
-            this.staggerAnimationDelay($(".snippet"));
-            $(".snippet").each(function(index, element) {    
+            var articleSnippet = document.querySelectorAll('.snippet')
+            this.staggerAnimationDelay(articleSnippet);
+
+            articleSnippet.forEach(function(element, index) {
 
                 new ScrollMagic.Scene({triggerHook: 1, triggerElement: element, offset: -250, reverse: false})
                     .setClassToggle(element, "snippet--reveal")
@@ -35,29 +32,27 @@ $(document).ready(function() {
                     .addTo(controller)
             });
 
-            // show recirc articles in article footer
-            // this scene is almost redundant
             new ScrollMagic.Scene({triggerHook: 1, triggerElement: ".article__content__footer", offset: 400, reverse: false})
                 .setClassToggle(".recirc__articles__item", "reveal")
                 // .addIndicators({name: "recirc"})
                 .addTo(controller)
 
             // show article share tools
-			new ScrollMagic.Scene({triggerHook: 0.05, triggerElement: ".article__content__title", reverse: false})
-	            .setClassToggle(".share-buttons__link-item", "share-buttons__link-item--reveal")
+            new ScrollMagic.Scene({triggerHook: 0.05, triggerElement: ".article__content__title", reverse: false})
+                .setClassToggle(".share-buttons__link-item", "share-buttons__link-item--reveal")
                 .addTo(controller);
 
             // Scale sticky header
-            new ScrollMagic.Scene({triggerHook: 0, duration: 100, offset: 10, reverse: true})
-                .setTween(".site-header", {height: "65px", boxShadow: "rgb(204, 204, 204) 0px 1px 3px;"})
+            new ScrollMagic.Scene({triggerHook: 0, offset: 10, reverse: true})
+                .setClassToggle(".site-header", "site-header--scaled")
                 // .addIndicators({name: "header (duration: 100)"})
                 .addTo(controller);
-            new ScrollMagic.Scene({triggerHook: 0, offset: 50, reverse: true})
+            new ScrollMagic.Scene({triggerHook: 0, offset: 10, reverse: true})
                 .setClassToggle(".site-header__logo", "site-header__logo--scaled")
                 // .addIndicators({name: "header__logo (duration: 0)"})
                 .addTo(controller);
             new ScrollMagic.Scene({triggerHook: 0, duration: 100, offset: 50, reverse: true})
-                .setTween(".navigation", {margin: "13px 30px 0 0"})
+                // .setTween(".navigation", {margin: "13px 30px 0 0"})
                 // .addIndicators({name: "navigation (duration: 100)"})
                 .addTo(controller);
             new ScrollMagic.Scene({triggerHook: 0, triggerElement: ".article__content__title", offset: 50, reverse: true})
@@ -67,7 +62,7 @@ $(document).ready(function() {
         },
 
         randomizeArticleHeaderColors: function() {
-            var $headerBackgroundElm = $(".article__no-feature-image");
+            var headerBackgroundElm = document.querySelector(".article__no-feature-image");
             var colorsArray = [
                 "#00704a",
                 "#b30838",
@@ -88,23 +83,21 @@ $(document).ready(function() {
             var randomBrandColor1 = colorsArray[getRandomInt(0, 8)];
             var randomBrandColor2 = colorsArray[getRandomInt(0, 8)];
 
-            $($headerBackgroundElm).css({background: "linear-gradient(to bottom right, " + randomBrandColor1 + ", " + randomBrandColor2});
+            headerBackgroundElm.style.background += "linear-gradient(to bottom right, " + randomBrandColor1 + ", " + randomBrandColor2;
+
         },
 
         articleAnimationTiming: function() {            
-            this.staggerAnimationDelay($(".share-buttons__link-item"));
-            this.staggerAnimationDelay($(".recirc__articles__item"));
+            this.staggerAnimationDelay(document.querySelectorAll(".share-buttons__link-item"));
+            this.staggerAnimationDelay(document.querySelectorAll(".recirc__articles__item"));
         },
 
-        staggerAnimationDelay: function($elmements) {
+        staggerAnimationDelay: function(elmements) {
             var delay = 0;
             var offset = 100;
 
-            $elmements.each(function(index){
-                $(this).css({
-                    'animation-delay': delay + 'ms'
-                });
-
+            elmements.forEach(function(element, index){
+                element.style.animationDelay = delay + 'ms';
                 // we only want to stagger what's visible and let scrollmagic triggers take care of the rest.
                 if (index < 3) {                    
                     delay += offset;
@@ -118,3 +111,4 @@ $(document).ready(function() {
     App.init();
 
 });
+
