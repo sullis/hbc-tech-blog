24950f8bd6630010bb2243cf58f0bab5373cbc72
designfirst <designfirst@gmail.com>
2017-12-05 10:43:23 -0500

    Update 2017-12-05T15:43:12.935Z

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,102 @@
 /* scripts */
 document.addEventListener('DOMContentLoaded',function() {
 
+    // GLOBAL VARS
+    var isNavOpen = false;
+    var isSearchOpen = false;
+
     var App = {
 
         init: function() {
             this.bindEvents();
-            LazyLoadRecircArticles.setupScenes();
-            LazyLoadRecircArticles.articleAnimationTiming();
+            LazyLoadArticleSnippets.setupScenes();
+            LazyLoadArticleSnippets.articleAnimationTiming();
+            SearchEvents.init();
         },
 
         bindEvents: function() {
+            document.getElementById('menu').addEventListener("click", NavEvents.toggleNav, false);
+            document.getElementById('header-search__svg').addEventListener("click", SearchEvents.toggleSearch, false);
+        },
 
+        setView: function(view) {
+
+            if (view === "search") {
+
+                var searchContainer = document.getElementById('header-search');
+                var searchInput = document.getElementById('header-search-input');
+                var searchResults = document.getElementById('header-search__results');
+
+                if (!isSearchOpen) {
+                    isSearchOpen = true;
+                    searchContainer.className += ' ' + 'header-search--active';
+                    searchInput.focus();
+
+                    if(isNavOpen) { this.setView("nav") };
+                } else {
+                    isSearchOpen = false;
+                    searchContainer.className = 'header-search';
+                    searchResults.innerHTML = '';
+                }
+
+            } else if (view === "nav") {
+
+                var navContainer = document.getElementById('menu');
+                
+                if (!isNavOpen) {
+                    isNavOpen = true;
+                    navContainer.className += ' ' + 'navigation--open';
+                    if(isSearchOpen) { this.setView("search") };
+                } else {
+                    isNavOpen = false;
+                    navContainer.className = 'navigation';
+                }
+            }
         }
-    }
+    };
 
-    var LazyLoadRecircArticles = {
+    var NavEvents = {
+
+        toggleNav: function(evt) {
+            App.setView("nav");
+        }
+    };
+
+    var SearchEvents = {
+
+        init: function() {
+
+            const siteSearch = new jekyllSearch(
+                '/search.json',
+                '#search-input',
+                '#search-results'
+            );
+
+            const headerSearch = new jekyllSearch(
+                '/search.json',
+                '#header-search-input',
+                '#header-search__results'
+            );
+
+            headerSearch.init();
+            siteSearch.init();
+        },
+
+        toggleSearch: function(evt) {
+            App.setView("search");
+        },
+
+        displayResults: function() {
+            // animate show reults
+            var results = document.querySelectorAll('.header-search--active__results .snippet')
+
+            results.forEach(function(element, index) {
+                element.className += ' ' + 'snippet--reveal';
+            });
+        }
+    };
+
+    var LazyLoadArticleSnippets = {
 
         setupScenes: function() {
             var controller = new ScrollMagic.Controller({globalSceneOptions:{refreshInterval: 0}});               
@@ -28,13 +110,13 @@ document.addEventListener('DOMContentLoaded',function() {
                 new ScrollMagic.Scene({triggerHook: 1, triggerElement: element, offset: -250, reverse: false})
                     .setClassToggle(element, "snippet--reveal")
                     // .addIndicators({name: "snippet"})
-                    .addTo(controller)
+                    .addTo(controller);
             });
 
             new ScrollMagic.Scene({triggerHook: 1, triggerElement: ".article__content__footer", offset: 400, reverse: false})
                 .setClassToggle(".recirc__articles__item", "reveal")
                 // .addIndicators({name: "recirc"})
-                .addTo(controller)
+                .addTo(controller);
 
             // show article share tools
             new ScrollMagic.Scene({triggerHook: 0.05, triggerElement: ".article__content__title", reverse: false})
@@ -74,7 +156,7 @@ document.addEventListener('DOMContentLoaded',function() {
                 if (index < 3) {                    
                     delay += offset;
                 } else {
-                    deslay = 0;
+                    delay = 0;
                 };
             });
         }
---
+++ assets/js/vendor/jekyll-search-js/search.js
@@ -0,0 +1,62 @@
+class jekyllSearch {
+    constructor(dataSource, searchField, resultsList) {
+        this.dataSource = dataSource;
+        this.searchField = document.querySelector(searchField);
+        this.resultsList = document.querySelector(resultsList);
+
+        this.displayResults = this.displayResults.bind(this);
+    }
+
+    fetchedData() {
+        return fetch(this.dataSource)
+        .then(blob => blob.json())
+    }
+
+    async findResults() {
+        const data = await this.fetchedData();
+        const regex = new RegExp(this.searchField.value, 'gi');
+        return data.filter(item => {
+            return item.title.match(regex) || item.excerpt.match(regex);
+        });
+    }
+
+    async displayResults() {
+        const results = await this.findResults();
+        const html = results.map(item => {
+            return `
+            <section class="snippet snippet--reveal">
+                <div class="snippet__meta">
+                    <a class="meta__category-link" href="/category/${item.category}">${item.category}</a>
+                    <span class="slug-divider"></span>
+                    <span class="meta__author">${item.author}</span>
+                    <span class="slug-divider"></span>
+                    <span class="meta__date">${item.date}</span>
+                </div>
+                <h1 class="snippet__title"><a class="snippet__title__link" title="${item.title}" href="${item.url}">${item.title}</a></h1>
+                <a class="snippet__excerpt__link" href="${item.url}"><p class="snippet__excerpt">${item.excerpt}</p></a>
+            </section>`;
+        }).join('');
+
+        this.resultsList.className += ' ' + 'header-search__results--active';
+
+        if ((results.length == 0) || (this.searchField.value == '')) {
+            this.resultsList.innerHTML = `<section class="snippet snippet--reveal"><p>Sorry, nothing was found</p></snippet>`;
+        } else {
+            this.resultsList.innerHTML = html;
+        }
+    }
+
+    async clearPlaceholder() {
+        this.searchField.placeholder = '';
+    }
+
+    init() {
+        this.searchField.addEventListener('click', this.clearPlaceholder.bind(this));
+        this.searchField.addEventListener('keyup', this.displayResults);
+        this.searchField.addEventListener('keypress', event => {
+            if (event.keyCode == 13) {
+                event.preventDefault();
+            }
+        });
+    }
+}
